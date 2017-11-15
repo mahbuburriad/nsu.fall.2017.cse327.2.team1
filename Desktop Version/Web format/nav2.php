5eee1906256a148a71c0fe6cd2e615e0d6961317
Md. Mahbubur Rahman <mahbubur.riad@gmail.com>
2017-11-15 22:07:45 +0600

    nav2 for admin panel

---
+++ Desktop Version/Web format/nav2.php
@@ -0,0 +1,94 @@
+<ul class="menuzord-menu">
+              <li><a href="admin-profile.php">Home</a></li>
+
+
+
+              <li><a href="#">About US</a>
+                <ul class="dropdown">
+                  <li><a href="#">AAAA</a></li>
+                  <li><a href="#">AAAAA</a></li>
+                  <li><a href="#">AAAAAAAAA</a>
+                    <ul class="dropdown">
+                      <li><a href="#">EAAAA</a></li>
+                      <li><a href="#">AAAAAA</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">AAAAAAAAAAAAAA</a>
+                    <ul class="dropdown">
+                      <li><a href="#">AAAAAAAAA</a></li>
+                      <li><a href="#">AAAAAAAAAA</a></li>
+                      <li><a href="#">AAAAAAAAAAA</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">AAAAAAAAAAAAA</a></li>
+
+                  <li><a href="#">AAAAAAAAAAAAA</a>
+                    <ul class="dropdown">
+                      <li><a href="#">AAAAAAAAAAAAA</a></li>
+                      <li><a href="#">AAAAAAAAAAAAA</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+
+
+
+              <li><a href="#">Help Page</a>
+                <ul class="dropdown">
+                  <li><a href="account.php">Account</a>
+                    <ul class="dropdown">
+                      <li><a href="account.php">Account</a></li>
+                      <li><a href="#">AAAAAAAAAA</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="all.php">All</a></li>
+                  <li><a href="#">SAr</a></li>
+                  <li><a href="#">OAAAA</a>
+                    <ul class="dropdown">
+                      <li><a href="page-pricing-style1.html">AA</a></li>
+                      <li><a href="page-pricing-style2.html">AAAAAA</a>
+                        <ul class="dropdown">
+                          <li><a href="#">AAAAAAAAA</a></li>
+                          <li><a href="#">AAAAAAA</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="#">AAAAAAAAAA</a></li>
+                      <li><a href="#">AAAAAAAAAAA</a></li>
+                      <li><a href="#">AAAAAAAAAAAA</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="page-calendar1.html">Calender</a></li>
+                </ul>
+              </li>
+
+
+
+              <li><a href="#">Portfolio </a>
+                <ul class="dropdown">
+                  <li><a href="#">2017 <span class="label label-info">New</span></a>
+                    <ul class="dropdown">
+                      <li><a href="#">Sub-menu01</a></li>
+                      <li><a href="#">Sub-menu02</a></li>
+                      <li><a href="#">Sub-menu03</a></li>
+                      <li><a href="#">Sub-menu04</a></li>
+                      <li><a href="#">Sub-menu05</a></li>
+                    </ul>
+                      </li>
+
+                      <li><a href="#">2016</a>
+                    <ul class="dropdown">
+                      <li><a href="#">Sub-menu01</a></li>
+                      <li><a href="#">Sub-menu02</a></li>
+                      <li><a href="#">Sub-menu03</a></li>
+                      <li><a href="#">Sub-menu04</a></li>
+                      <li><a href="#">Sub-menu05</a></li>
+                    </ul>
+                      </li>
+                      
+                </ul>
+              </li>
+              
+                <li><a href="contact-us.php">Contact Us</a></li>
+              </li>
+            </ul>
+          </div>
