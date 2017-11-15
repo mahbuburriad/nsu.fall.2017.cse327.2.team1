dc52a25471ba11ea34dfeeae0f86077383f28b3b
Md. Mahbubur Rahman <mahbubur.riad@gmail.com>
2017-11-15 22:04:20 +0600

    footer for admin profile
    there are some js and css added in footer2. for admin profile

---
+++ Desktop Version/Web format/footer2.php
@@ -0,0 +1,51 @@
+ <!-- Footer -->
+ 
+  <footer id="footer" class="footer" data-bg-color="#1f1f1f">
+    <div class="container pt-60 pb-40">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 text-center">
+          <img style="height: 60px; width: 90px" src="images/logo-wide.jpg" alt="">
+          <p class="font-12 mt-20 mb-20">The Admin Panel, here admin can edit their file and controll all functions</p>
+          <ul class="styled-icons">
+            <li><a href="#"><i class="fa fa-facebook"></i></a> </li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a> </li>
+            <li><a href="#"><i class="fa fa-pinterest"></i></a> </li>
+            <li><a href="#"><i class="fa fa-google-plus"></i></a> </li>
+            <li><a href="#"><i class="fa fa-youtube"></i></a> </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom" data-bg-color="#2f2f2f">
+      <div class="container pt-15 pb-10">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <p class="font-11 text-black-777 m-0">Copyright &copy;2017 Automated Accommodation. All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+</div>
+<!-- end wrapper -->
+
+<!-- Footer Scripts -->
+<!-- JS | Custom script for all pages -->
+<script src="js/custom.js"></script>
+
+<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  
+      (Load Extensions only on Local File Systems ! 
+       The following part can be removed on Server for On Demand Loading) -->
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.actions.min.js"></script>
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"></script>
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.migration.min.js"></script>
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"></script>
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
+
+</body>
+</html>
