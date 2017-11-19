af058076cc456dd09f02cc6003e5fbe1bdb56bda
Md. Mahbubur Rahman <mahbubur.riad@gmail.com>
2017-11-19 18:55:02 +0600

    testimonial.php is not team profile
    this is for what people said about us. so updated the whole testimonial.php

--- Desktop Version/Web format/testimonial.php
+++ Desktop Version/Web format/testimonial.php
@@ -15,44 +15,44 @@
               <div class="item">
                 <div class="testimonial pt-10">
                   <div class="thumb pull-left mb-0 mr-0 pr-20">
-                    <img style="height: 100px; width: 100px" class="img-circle" alt="" src="Images/riad.jpg">
+                    <img style="height: 100px; width: 100px" class="img-circle" alt="" src="Images/test01.jpg">
                   </div>
                   <div class="ml-100 ">
-                    <h4 class="text-white mt-0">Admin,</h4>
-                    <p class="author mt-20">- <span class="text-theme-color-2">Mahbubur Riad,</span> <small><em class="text-gray-lightgray">Admin, Automated Accomodation.</em></small></p>
+                    <h4 class="text-white mt-0">This is very good site and helps to solve all problems about Accommodation</h4>
+                    <p class="author mt-20">- <span class="text-theme-color-2">Bill Gates</span> <small><em class="text-gray-lightgray">CEO, Microsoft</em></small></p>
                   </div>
                 </div>
               </div>
               <div class="item">
                 <div class="testimonial pt-10">
                   <div class="thumb pull-left mb-0 mr-0 pr-20">
-                    <img style="height: 100px; width: 100px" class="img-circle" alt="" src="Images/shakil.jpg">
+                    <img style="height: 100px; width: 100px" class="img-circle" alt="" src="Images/test02.jpg">
                   </div>
                   <div class="ml-100 ">
-                    <h4 class="text-white mt-0">CEO,</h4>
-                    <p class="author mt-20">- <span class="text-theme-color-2">Shakilur Rahman,</span> <small><em class="text-gray-lightgray">CEO, Automated Accomodation.</em></small></p>
+                    <h4 class="text-white mt-0">very good in all accoommodation purpose</h4>
+                    <p class="author mt-20">- <span class="text-theme-color-2">Jeff Bezos</span> <small><em class="text-gray-lightgray">CEO, Amazon</em></small></p>
                   </div>
                 </div>
               </div>
               <div class="item">
                 <div class="testimonial pt-10">
                   <div class="thumb pull-left mb-0 mr-0 pr-20">
-                    <img style="height: 100px; width: 100px" class="img-circle" alt="" src="Images/aziz.jpg">
+                    <img style="height: 100px; width: 100px" class="img-circle" alt="" src="Images/test03.jpg">
                   </div>
                   <div class="ml-100 ">
-                    <h4 class="text-white mt-0">Adviser,</h4>
-                    <p class="author mt-20">- <span class="text-theme-color-2">Azizur Rahman,</span> <small><em class="text-gray-lightgray">Adviser, Automated Accomodation.</em></small></p>
+                    <h4 class="text-white mt-0">Very good at social service with accommodation</h4>
+                    <p class="author mt-20">- <span class="text-theme-color-2">Carlos slim helu</span> <small><em class="text-gray-lightgray">CEO, Pinrest</em></small></p>
                   </div>
                 </div>
               </div>
               <div class="item">
                 <div class="testimonial pt-10">
                   <div class="thumb pull-left mb-0 mr-0 pr-20">
-                    <img style="height: 100px; width: 100px" class="img-circle" alt="" src="Images/joydip.jpg">
+                    <img style="height: 100px; width: 100px" class="img-circle" alt="" src="Images/test04.jpg">
                   </div>
                   <div class="ml-100 ">
-                    <h4 class="text-white mt-0">Vise Controller,</h4>
-                    <p class="author mt-20">- <span class="text-theme-color-2">Joydip Chowdhury,</span> <small><em class="text-gray-lightgray">Vise Controller, Automated Accomodation.</em></small></p>
+                    <h4 class="text-white mt-0">Very good in all services</h4>
+                    <p class="author mt-20">- <span class="text-theme-color-2">Warren Buffett</span> <small><em class="text-gray-lightgray">CEO, Warren Buffett & Co.</em></small></p>
                   </div>
                 </div>
               </div>
