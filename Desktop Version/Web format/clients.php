5edf546732d9543874e191f4a5d5d63c250a2f15
Md. Mahbubur Rahman <mahbubur.riad@gmail.com>
2017-11-19 19:04:28 +0600

    Sponsor List Added and modifed

--- Desktop Version/Web format/clients.php
+++ Desktop Version/Web format/clients.php
@@ -5,16 +5,16 @@
           <div class="col-md-12">
             <!-- Section: Clients -->
             <div class="owl-carousel-6col clients-logo transparent text-center owl-nav-top">
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="http://www.tedthomas.com/wp-content/uploads/2017/01/home1.png" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s1.jpg" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s2.jpg" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s3.jpg" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s4.png" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s5.jpg" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s6.jpg" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s7.png" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s8.jpg" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s9.jpg" alt=""></a></div>
+              <div class="item"> <a href="#"><img style="height: 120px; width: 200px" src="Images/s10.jpg" alt=""></a></div>
             </div>
           </div>
         </div>
