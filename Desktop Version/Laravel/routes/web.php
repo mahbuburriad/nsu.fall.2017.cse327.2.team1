629ce3c9ef534e49134507fd46dcc6918d835d4a
Nazmul Hasan <edufornazmul@gmail.com>
2017-11-28 02:25:27 +0600

    Route fixed for newly added pages

--- Desktop Version/Laravel/routes/web.php
+++ Desktop Version/Laravel/routes/web.php
@@ -7,7 +7,7 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group.
 |
 */
 
@@ -21,6 +21,10 @@ Route::get('/login',[
 	'uses'=>'pagesController@login'
 ]);
 
+Route::get('/home',[
+	'as' => 'home',
+	'uses'=>'pagesController@home'
+]);
 Route::post('register', [ 
 	'as' => 'register', 
 	'uses' => 'Auth\RegisterController@store'
@@ -46,3 +50,32 @@ Route::get('/social/handle/{provider}',
 
 ]); 
 
+Route::get('/contact',[
+	'as' => 'contact',
+	'uses'=>'pagesController@contact'
+]);
+
+Route::get('/about',[
+	'as' => 'about',
+	'uses'=>'pagesController@about'
+]);
+
+Route::get('/gallery',[
+	'as' => 'gallery',
+	'uses'=>'pagesController@gallery'
+]);
+
+Route::get('/account',[
+	'as' => 'account',
+	'uses'=>'pagesController@account'
+]);
+
+Route::get('/editprofile',[
+	'as' => 'editprofile',
+	'uses'=>'pagesController@editprofile'
+]);
+
+Route::get('/logout',function() {
+	Auth::logout();
+	return Redirect::to('/');
+})->middleware('auth');
