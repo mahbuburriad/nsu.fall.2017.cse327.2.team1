084852c6daca8b1b9c99e7958caef76e968824bc
Md. Mahbubur Rahman <mahbubur.riad@gmail.com>
2017-11-29 17:35:55 +0600

    contact us blade file added in laravel

---
+++ Desktop Version/Laravel2/resources/views/contact-us.blade.php
@@ -0,0 +1,152 @@
+<?php
+require_once ('header1.php');
+?>
+<title>Automated Accommodation || Contact US</title>
+<?php
+require_once ('header2.php');
+?>
+  <div class="main-content">
+
+    <!-- Section: inner-header -->
+    <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="images/pagetitle.jpg">
+      <div class="container pt-70 pb-20">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="title text-white">Contact US</h2>
+              <ol class="breadcrumb text-left text-black mt-10">
+                <li><a href="#">Home</a></li>
+                <li class="active text-gray-silver">Contact US</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+   
+ <!-- Divider: Contact -->
+    <section class="divider layer-overlay overlay-white-9" data-bg-img="images/bg/bg15.jpg">
+      <div class="container">
+        <div class="row pt-30">
+          <div class="col-md-8">
+            <h3 class="line-bottom mt-0 mb-20">Interested in discussing?</h3>
+            <!-- Contact Form -->
+            <form id="contact_form" name="contact_form" class="form-transparent" action="includes/sendmail.php" method="post">
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="form_name">Name <small>*</small></label>
+                    <input style="color: #002e5b" name="form_name" class="form-control" type="text" placeholder="Enter Name" required="">
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="form_email">Email <small>*</small></label>
+                    <input style="color: #002e5b"  name="form_email" class="form-control required email" type="email" placeholder="Enter Email">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="form_name">Subject <small>*</small></label>
+                    <input style="color: #002e5b"  name="form_subject" class="form-control required" type="text" placeholder="Enter Subject">
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="form_phone">Phone</label>
+                    <input style="color: #002e5b"  name="form_phone" class="form-control" type="text" placeholder="Enter Phone">
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="form_name">Message</label>
+                <textarea style="color: #002e5b"  name="form_message" class="form-control required" rows="5" placeholder="Enter Message"></textarea>
+              </div>
+              <div class="form-group">
+                <input name="form_botcheck" class="form-control" type="hidden" value="" />
+                <button type="submit" class="btn btn-dark btn-theme-colored btn-flat mr-5" data-loading-text="Please wait...">Send your message</button>
+                <button type="reset" class="btn btn-default btn-flat btn-theme-colored">Reset</button>
+              </div>
+            </form>
+            <!-- Contact Form Validation-->
+            <script type="text/javascript">
+              $("#contact_form").validate({
+                submitHandler: function(form) {
+                  var form_btn = $(form).find('button[type="submit"]');
+                  var form_result_div = '#form-result';
+                  $(form_result_div).remove();
+                  form_btn.before('<div id="form-result" class="alert alert-success" role="alert" style="display: none;"></div>');
+                  var form_btn_old_msg = form_btn.html();
+                  form_btn.html(form_btn.prop('disabled', true).data("loading-text"));
+                  $(form).ajaxSubmit({
+                    dataType:  'json',
+                    success: function(data) {
+                      if( data.status == 'true' ) {
+                        $(form).find('.form-control').val('');
+                      }
+                      form_btn.prop('disabled', false).html(form_btn_old_msg);
+                      $(form_result_div).html(data.message).fadeIn('slow');
+                      setTimeout(function(){ $(form_result_div).fadeOut('slow') }, 6000);
+                    }
+                  });
+                }
+              });
+            </script>
+          </div>
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="icon-box left media bg-black-333 p-25 mb-20"> <a class="media-left pull-left" href="#"> <i class="pe-7s-map-2 text-theme-color-2"></i></a>
+                  <div class="media-body"> <strong class="text-white">OUR OFFICE LOCATION</strong>
+                    <p class="text-white">North South University Road, Bashundhara Residential area, Dhaka</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-6 col-md-12 text-white">
+                <div class="icon-box left media bg-black-333 p-25 mb-20"> <a class="media-left pull-left" href="#"> <i class="pe-7s-call text-theme-color-2"></i></a>
+                  <div class="media-body"> <strong class="text-white">OUR CONTACT NUMBER</strong>
+                    <p>+880 12345 65478</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-6 col-md-12 text-white">
+                <div class="icon-box left media bg-black-333 p-25 mb-20"> <a class="media-left pull-left" href="#"> <i class="pe-7s-mail text-theme-color-2"></i></a>
+                  <div class="media-body"> <strong class="text-white">OUR CONTACT E-MAIL</strong>
+                    <p>supporte@aa.com</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-6 col-md-12 text-white">
+                <div class="icon-box left media bg-black-333 p-25 mb-20"> <a class="media-left pull-left" href="#"> <i class="fa fa-skype text-theme-color-2"></i></a>
+                  <div class="media-body"> <strong class="text-white">Make a Video Call</strong>
+                    <p>accommodationSkype</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <!-- Divider: Google Map -->
+    <section>
+      <div class="container-fluid p-0">
+        <div class="row">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.0983083934116!2d90.42334931552533!3d23.8151029845574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c64c103a8093%3A0xd660a4f50365294a!2sNorth+South+University!5e0!3m2!1sen!2sbd!4v1510865355935" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        </div>
+      </div>
+    </section>
+  </div>
+  <!-- end main-content -->
+
+
+  <?php
+require_once ('footer.php');
+?>
