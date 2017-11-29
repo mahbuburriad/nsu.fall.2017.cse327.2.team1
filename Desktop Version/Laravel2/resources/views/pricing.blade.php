b59add52b2ed77e147ab56c124ba62867f0eeea8
Md. Mahbubur Rahman <mahbubur.riad@gmail.com>
2017-11-29 17:35:15 +0600

    pricing table blade file added in laravel

---
+++ Desktop Version/Laravel2/resources/views/pricing.blade.php
@@ -0,0 +1,97 @@
+<?php
+require_once ('header1.php');
+?>
+<title>Automated Accommodation || Login & Register</title>
+<?php
+require_once ('header2.php');
+?>
+  <div class="main-content">
+
+    <!-- Section: inner-header -->
+    <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="images/pagetitle.jpg">
+      <div class="container pt-70 pb-20">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="title text-white">Price</h2>
+              <ol class="breadcrumb text-left text-black mt-10">
+                <li><a href="#">Home</a></li>
+                <li class="active text-gray-silver">Pricing</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<!-- Section: Pricing -->
+    <section id="pricing" data-bg-img="images/pattern/p4.png">
+      <div class="container">
+        <div class="section-content">
+          <div class="row">
+            <div class="col-xs-12 col-sm-4 col-md-4 hvr-float-shadow mb-sm-30">
+              <div class="pricing-table">
+                <div class="font-36 pl-20 bg-theme-color-2 text-white text-left pr-20 p-10"><sup>$</sup>200 <span class="font-15 pull-right mt-15 text-white">Per/Year</span>
+                </div>
+                <div class=" bg-lighter border-1px p-30 pt-20 pb-20">
+                <h3 class="package-type font-28 m-0 text-black">Basic</h3>
+                <ul class="table-list list-icon theme-colored pb-0">
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-times"></i>Can't Search</li>
+                  <li><i class="fa fa-times"></i>Can't Search</li>
+                </ul>
+                </div>
+                <a href="#" class="btn btn-lg btn-theme-colored text-uppercase btn-block pt-20 pb-20 btn-flat">Signup</a>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4 hvr-float-shadow mb-sm-30">
+              <div class="pricing-table">
+                <div class="font-36 pl-20 bg-theme-color-2 text-white text-left pr-20 p-10"><sup>$</sup>390 <span class="font-15 pull-right mt-15 text-white">Per/Year</span>
+                </div>
+                <div class=" bg-lighter border-1px p-30 pt-20 pb-20">
+                <h3 class="package-type font-28 m-0 text-black">Standerd</h3>
+                <ul class="table-list list-icon theme-colored pb-0">
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-times"></i>Can't Search</li>
+                  <li><i class="fa fa-times"></i>Can't Search</li>
+                </ul>
+                </div>
+                <a href="#" class="btn btn-lg btn-theme-colored text-uppercase btn-block pt-20 pb-20 btn-flat">Signup</a>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4 hvr-float-shadow mb-sm-30">
+              <div class="pricing-table">
+                <div class="font-36 pl-20 bg-theme-color-2 text-white text-left pr-20 p-10"><sup>$</sup>570 <span class="font-15 pull-right mt-15 text-white">Per/Year</span>
+                </div>
+                <div class=" bg-lighter border-1px p-30 pt-20 pb-20">
+                <h3 class="package-type font-28 m-0 text-black">Premium</h3>
+                <ul class="table-list list-icon theme-colored pb-0">
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-check"></i>Can Search</li>
+                  <li><i class="fa fa-times"></i>Can't Search</li>
+                  <li><i class="fa fa-times"></i>Can't Search</li>
+                </ul>
+                </div>
+                <a href="#" class="btn btn-lg btn-theme-colored text-uppercase btn-block pt-20 pb-20 btn-flat">Signup</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <!-- end main-content -->
+
+
+    <?php
+require_once ('footer.php');
+?>
