dc765c8d134ee5f259e93ce07ccd9ed89fbd56c4
Md. Mahbubur Rahman <mahbubur.riad@gmail.com>
2017-11-29 16:43:26 +0600

    set index route to laravel

--- Desktop Version/Laravel2/routes/web.php
+++ Desktop Version/Laravel2/routes/web.php
@@ -9,13 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 |
 */
 
 
-Route::get('/index', function () {
+Route::get('/', function () {
     return view('index');
 });
