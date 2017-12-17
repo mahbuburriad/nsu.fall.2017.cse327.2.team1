2e5d36bdca9acf6d5841710fe7b853b0e6eb0a2f
Nazmul Hasan <edufornazmul@gmail.com>
2017-12-17 22:37:18 +0600

    css and js updated

---
+++ Desktop Version/Laravel/public/autoacco/Help Page/how to add page.php
@@ -0,0 +1,40 @@
+<?php
+require_once ('header1.php');
+?>
+<title>Automated Accommodation || Login & Register</title> <!-- change the name of that you want to add, this is main page title -->
+<?php
+require_once ('header2.php');
+?>
+  <div class="main-content">
+
+    <!-- Section: inner-header -->
+    <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="images/pagetitle.jpg">
+      <div class="container pt-70 pb-20">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="title text-white">Login/Register</h2> <!-- change the name of that you want to add -->
+              <ol class="breadcrumb text-left text-black mt-10">
+                <li><a href="#">Home</a></li> <!-- change to sub title or something another or link -->
+                <li class="active text-gray-silver">Login & Register</li> <!-- change to sub title or something another or link -->
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section>
+      <div class="container">
+        <div class="row">
+
+        </div>
+      </div>
+    </section>
+  </div>
+    <!-- end main-content -->
+<?php
+require_once ('footer.php');
+?>
